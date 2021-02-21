fb8cbf7ed8b52019420c994d3e519cc11f96d9ce
Joseph Chan <josephchan@Josephs-MBP.hitronhub.home>
2021-02-21 02:15:43 -0800

    Added a login and background image for login screen

--- stormhacks_app/lib/loginScreen.dart
+++ stormhacks_app/lib/loginScreen.dart
@@ -7,9 +7,96 @@ class login extends StatefulWidget {
   _loginState createState() => _loginState();
 }
 
-class _loginState extends State<login> {
+class _loginState extends State<login> with SingleTickerProviderStateMixin{
+
+  AnimationController _iconAnimationController;
+  Animation<double> _iconAnimation;
+
+@override
+void initState(){
+  super.initState();
+  _iconAnimationController = new AnimationController(
+  vsync: this,
+    duration: new Duration(milliseconds: 500)
+  ); 
+  _iconAnimation = new CurvedAnimation(
+    parent: _iconAnimationController,
+    curve: Curves.easeOut
+  );
+  _iconAnimation.addListener(()=> this.setState((){}));
+  _iconAnimationController.forward();
+}
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return new Scaffold(
+      backgroundColor: Colors.blue,
+      body: new Stack
+      (
+        fit: StackFit.expand,
+        children: <Widget>
+        [
+          new Image
+          (
+            image: new AssetImage("assets/images/cool.jpeg"),
+            fit: BoxFit.cover,
+            colorBlendMode: BlendMode.darken
+          ), 
+          new Column
+          (
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>
+            [
+              new FlutterLogo
+              (
+              size: _iconAnimation.value * 100,
+              ), 
+              new Form(
+                child: new Theme(
+                  data: new ThemeData(
+                    brightness: Brightness.dark,
+                    primarySwatch: Colors.green, 
+                    inputDecorationTheme: new InputDecorationTheme(
+                      labelStyle: new TextStyle(
+                        color: Colors.white, fontSize: 20.0))),
+                      child: new Container(
+                        padding: const EdgeInsets.all(40.0),
+                          child: new Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new TextFormField( 
+                        decoration: new InputDecoration(
+                        labelText: "Enter Email",
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        ),
+                    new TextFormField(
+                        decoration: new InputDecoration(
+                        hintText: "Enter Password",
+                        ),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                        ),
+                        new Padding(
+                          padding: const EdgeInsets.only(top:20.0),
+                          ),
+                      new MaterialButton(
+                        color: Colors.black,
+                        textColor: Colors.white,
+                        child: new Text("Login"),
+                        onPressed: ()=> {
+                          Navigator.pushReplacementNamed(context, '/home')
+                         },
+                        splashColor: Colors.greenAccent,
+                         ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ], 
+          ) 
+        ], 
+      ),  
+    );  
   }
 }
