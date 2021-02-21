b055b7270c29d2ee4dcafae820b2f090b83dc0ba
Tyler Trinh <tylervtrinh@gmail.com>
2021-02-20 17:26:36 -0800

    feat: use private helper methods

--- stormhacks_app/lib/loginScreen.dart
+++ stormhacks_app/lib/loginScreen.dart
@@ -19,51 +19,55 @@ class _loginState extends State<login> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          userPrompt,
+          _buildUserPrompt(),
           Text("Today's Meals", style: TextStyle(fontSize: 20)),
-          todaysMeals
+          _buildTodaysMeals()
         ],
       ),
     ));
   }
 
-  Widget userPrompt = Container(
-    margin: const EdgeInsets.only(bottom: 50),
-    width: 400,
-    height: 100,
-    color: Colors.amber[600],
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          "Hi Jordan",
-          style: TextStyle(fontSize: 40),
-        ),
-        Text(
-          "You have 12 tokens.",
-          style: TextStyle(fontSize: 18),
-        ),
-      ],
-    ),
-  );
-
-  Widget todaysMeals = Container(
-      margin: const EdgeInsets.only(top: 10),
-      decoration:
-          BoxDecoration(border: Border.all(color: Colors.black, width: 3)),
+  Container _buildUserPrompt() {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 50),
       width: 400,
-      height: 175,
-      child: Container(
-          margin: const EdgeInsets.only(top: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(height: 80, width: 80, color: Colors.grey),
-              Container(height: 80, width: 80, color: Colors.grey),
-              Container(height: 80, width: 80, color: Colors.grey),
-            ],
-          )));
+      height: 100,
+      color: Colors.amber[600],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            "Hi Jordan",
+            style: TextStyle(fontSize: 40),
+          ),
+          Text(
+            "You have 12 tokens.",
+            style: TextStyle(fontSize: 18),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _buildTodaysMeals() {
+    return Container(
+        margin: const EdgeInsets.only(top: 10),
+        decoration:
+            BoxDecoration(border: Border.all(color: Colors.black, width: 3)),
+        width: 400,
+        height: 175,
+        child: Container(
+            margin: const EdgeInsets.only(top: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildMeal(80.0, Colors.grey),
+                _buildMeal(80.0, Colors.grey),
+                _buildMeal(80.0, Colors.grey),
+              ],
+            )));
+  }
 
   Container _buildMeal(double dimension, Color color) {
     return Container(height: dimension, width: dimension, color: color);
