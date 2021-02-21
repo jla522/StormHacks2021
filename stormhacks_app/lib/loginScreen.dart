4acaf79644c21c1d3089b9b8e1dc6fd61c4b322a
Tyler Trinh <tylervtrinh@gmail.com>
2021-02-20 17:18:25 -0800

    feat: display user prompt and todays meals

--- stormhacks_app/lib/loginScreen.dart
+++ stormhacks_app/lib/loginScreen.dart
@@ -8,8 +8,64 @@ class login extends StatefulWidget {
 }
 
 class _loginState extends State<login> {
+  String username = "Jordan";
+  int numTokens = 12;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Container(
+      margin: const EdgeInsets.fromLTRB(30, 70, 30, 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          userPrompt,
+          Text("Today's Meals", style: TextStyle(fontSize: 20)),
+          todaysMeals
+        ],
+      ),
+    ));
+  }
+
+  Widget userPrompt = Container(
+    margin: const EdgeInsets.only(bottom: 50),
+    width: 400,
+    height: 100,
+    color: Colors.amber[600],
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          "Hi Jordan",
+          style: TextStyle(fontSize: 40),
+        ),
+        Text(
+          "You have 12 tokens.",
+          style: TextStyle(fontSize: 18),
+        ),
+      ],
+    ),
+  );
+
+  Widget todaysMeals = Container(
+      margin: const EdgeInsets.only(top: 10),
+      decoration:
+          BoxDecoration(border: Border.all(color: Colors.black, width: 3)),
+      width: 400,
+      height: 175,
+      child: Container(
+          margin: const EdgeInsets.only(top: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(height: 80, width: 80, color: Colors.grey),
+              Container(height: 80, width: 80, color: Colors.grey),
+              Container(height: 80, width: 80, color: Colors.grey),
+            ],
+          )));
+
+  Container _buildMeal(double dimension, Color color) {
+    return Container(height: dimension, width: dimension, color: color);
   }
 }
