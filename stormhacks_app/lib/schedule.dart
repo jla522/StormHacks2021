a6dcf06a0a45e7c93a8673cd1b4096b061c3cec0
alvindav <alvind@sfu.ca>
2021-02-21 03:32:59 -0800

    basis for adding pictures to schedule.dart

--- stormhacks_app/lib/schedule.dart
+++ stormhacks_app/lib/schedule.dart
@@ -18,25 +18,26 @@ class _scheduleState extends State<schedule> {
 		return Scaffold(
 			bottomNavigationBar: navBar(),
 			body: ListView(
-			children: <Widget>[
-				Container(
-					margin: const EdgeInsets.fromLTRB(30, 70, 30, 30),
-					child: Column(
-						crossAxisAlignment: CrossAxisAlignment.start,
-						children: [
-							Text("Schedule (MON-FRI)", style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold)),	
-							daySection(currentDay, currentDayRestaurants, currentDayDeals),
-							for(int i = 0; i < daysOfWeek.length; i++) daySection(daysOfWeek[i], currentDayRestaurants, currentDayDeals),
-						],
-					), 
-				),	
-			],
-			),
-		);
-  }
-}
+				children: <Widget>[
+					Container(
+						margin: const EdgeInsets.fromLTRB(30, 70, 30, 30),
+						child: Column(
+							crossAxisAlignment: CrossAxisAlignment.start,
+							children: [
+								Text("Schedule (MON-FRI)", style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold)),	
+								daySection(currentDay, currentDayRestaurants, currentDayDeals),
+								for(int i = 0; i < daysOfWeek.length; i++) daySection(daysOfWeek[i], currentDayRestaurants, currentDayDeals),
+							],//children
+						),//column
+					),//container	
+				],//children
+			),//listview
+		);//scaffold
+	}//Widget Build
+}//class
 
-class daySection extends StatelessWidget{
+class daySection extends StatelessWidget{ 
+//daySection creates the day string (i.e. Tuesday), and passes in a list of restaurants and their deals for that given day to class:foodDeal
 	final String _day;
 	final List<String> _restaurants;
 	final List<String> _deals;
@@ -62,18 +63,31 @@ class daySection extends StatelessWidget{
 }//daySection
 
 class foodDeal extends StatelessWidget {
+//foodDeal prints out a picture of the restaurant, the restaurant string, and their deal.
 	final String _restaurant;
 	final String _deal;
 	
 	foodDeal(this._restaurant, this._deal);
 	@override
 	Widget build(BuildContext context){
-		return Column(
-			crossAxisAlignment: CrossAxisAlignment.start,
+		return Row(
+			mainAxisAlignment: MainAxisAlignment.start,
 			children: <Widget>[
-				Text(_restaurant, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-				Text(_deal, style: TextStyle(fontSize: 12) ),
-				//Divider(color: Colors.black),
+				Container(
+					height: 60.0,
+					width: 60.0,
+					decoration: BoxDecoration( image: DecorationImage(image: AssetImage('assets/images/settings.png'), fit: BoxFit.fill)),
+				),
+				Container(
+					margin: const EdgeInsets.fromLTRB(30, 0, 0, 0),
+					child: Column(
+						crossAxisAlignment: CrossAxisAlignment.start,
+						children: <Widget>[
+							Text(_restaurant, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+							Text(_deal, style: TextStyle(fontSize: 12) ),
+						],//children
+					), //column
+				),//container
 			], //children
 		); //Column
 	}//build
